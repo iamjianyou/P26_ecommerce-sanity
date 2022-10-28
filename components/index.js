0645562698f976a742676b2f4df5d9fd9af6bec4
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-10-28 22:16:20 +0200

    add sanity project

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar.component';
+export { default as Cart } from './Cart.component';
+export { default as Product } from './Product.component';
+export { default as HeroBanner } from './HeroBanner.component';
+export { default as FooterBanner} from './Footer.component';
+export { default as Layout } from './Layout.component';
